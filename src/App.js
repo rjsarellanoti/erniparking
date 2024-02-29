90f9eeeae5fc34fe6586e3db6b8dc1104ddfbdbf
rjsarellanoti <ryan.arellano@telusinternational.com>
2024-03-01 05:45:07 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import pgiImage from './assets/images/pgi.jpg'
+
+
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+
+    <Container>
+      
+      <Row>
+
+      <Col md={4} className="my-2">
+               <Card style={{ width: '40%', margin: '10px', }}>
+                <Card.Img variant="top" src={pgiImage} />
+                <Card.Body>
+                <Card.Title>Gate 1</Card.Title>
+                  <Card.Text>
+                    Click to Enter gate 1
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+        </Col>
+
+
+        <Col md={4} className="my-2">
+              <Card style={{ width: '40%', margin: '10px' }}>
+                <Card.Img variant="top" src={pgiImage} />
+                <Card.Body>
+                <Card.Title>Gate 2</Card.Title>
+                  <Card.Text>
+                    Click to Enter gate 2
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+        </Col>
+
+
+        <Col md={4} className="my-2">
+              <Card style={{ width: '40%', margin: '10px' }}>
+                <Card.Img variant="top" src={pgiImage} />
+                <Card.Body>
+                  <Card.Title>Gate 3</Card.Title>
+                  <Card.Text>
+                    Click to Enter gate 3
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+        </Col>
+        
+        </Row>
+        </Container>
   );
 }
 
